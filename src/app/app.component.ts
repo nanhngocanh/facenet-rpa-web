149287f7fc883a0d913bd31beb94e29842f289a8
unknown <ngocanhanh>
2022-05-24 14:14:48 +0700

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,54 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  thumbnail_post : any[] = [
-    {
-      thumbnail_category: 'Article',
-      picture_link: 'https://sisuadigital.com/wp-content/uploads/2022/05/working-on-laptop-300x200.jpg',
-      thumbnail_title: 'INVOICE PROCESS AUTOMATION WITH AI AND RPA',
-      thumbnail_content: 'Processing invoices is mandatory yet time and resource consuming task for organizations. Read on as we discuss how RPA and AI has boosted our customers’ invoice processing.',
-      thumbnail_date: '06/05/2022'
-    },
-    {
-      thumbnail_category: 'Article',
-      picture_link: 'https://sisuadigital.com/wp-content/uploads/2022/05/working-on-laptop-300x200.jpg',
-      thumbnail_title: 'INVOICE PROCESS AUTOMATION WITH AI AND RPA',
-      thumbnail_content: 'Processing invoices is mandatory yet time and resource consuming task for organizations. Read on as we discuss how RPA and AI has boosted our customers’ invoice processing.',
-      thumbnail_date: '06/05/2022'
-    },
-    {
-      thumbnail_category: 'Article',
-      picture_link: 'https://sisuadigital.com/wp-content/uploads/2022/05/working-on-laptop-300x200.jpg',
-      thumbnail_title: 'INVOICE PROCESS AUTOMATION WITH AI AND RPA',
-      thumbnail_content: 'Processing invoices is mandatory yet time and resource consuming task for organizations. Read on as we discuss how RPA and AI has boosted our customers’ invoice processing.',
-      thumbnail_date: '06/05/2022'
-    },
-    {
-      thumbnail_category: 'Article',
-      picture_link: 'https://sisuadigital.com/wp-content/uploads/2022/05/working-on-laptop-300x200.jpg',
-      thumbnail_title: 'INVOICE PROCESS AUTOMATION WITH AI AND RPA',
-      thumbnail_content: 'Processing invoices is mandatory yet time and resource consuming task for organizations. Read on as we discuss how RPA and AI has boosted our customers’ invoice processing.',
-      thumbnail_date: '06/05/2022'
-    }
-  ];
-
-  twitter_post : any[] = [
-    {
-      publish_time: '2 weeks ago',
-      twitter_content: 'How to make an app with Power Apps? Watch our video (in finnish) which shows just how fast you can make an app: Power Apps is part of Microsoft\'s Power Platform which includes different low-code tools. Read more on our website:'
-    },
-    {
-      publish_time: '2 weeks ago',
-      twitter_content: 'How to make an app with Power Apps? Watch our video (in finnish) which shows just how fast you can make an app: Power Apps is part of Microsoft\'s Power Platform which includes different low-code tools. Read more on our website:'
-    },
-    {
-      publish_time: '2 weeks ago',
-      twitter_content: 'How to make an app with Power Apps? Watch our video (in finnish) which shows just how fast you can make an app: Power Apps is part of Microsoft\'s Power Platform which includes different low-code tools. Read more on our website:'
-    }
-  ];
-
-
+  title = 'facenetrpa';
 }
