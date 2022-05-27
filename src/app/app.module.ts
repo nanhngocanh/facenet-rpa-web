d792481d1f13c6c13705b9d96063ea5a19d4d7d2
unknown <ngocanh732001@gmail.com>
2022-05-27 17:42:24 +0700

    update api insight

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -45,7 +46,7 @@ import { RouterModule } from '@angular/router';
     InsightComponent,
     ContactComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/insight/insight.component.ts
+++ src/app/pages/insight/insight.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-insight',
   templateUrl: './insight.component.html',
-  styleUrls: ['./insight.component.css']
+  styleUrls: ['./insight.component.css'],
 })
 export class InsightComponent implements OnInit {
-
-  constructor() { }
+  items: any;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get('http://192.168.1.253:8080/limitinsights')
+      .subscribe((data) => {
+        this.items = data;
+      });
   }
-
 }
