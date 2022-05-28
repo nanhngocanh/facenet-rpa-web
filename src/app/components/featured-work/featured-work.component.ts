7c47c9aef03289e9b2b24de2a4475e35b4b8bec4
unknown <ngocanh732001@gmail.com>
2022-05-28 13:21:43 +0700

    update homepage

--- src/app/components/featured-work/featured-work.component.ts
+++ src/app/components/featured-work/featured-work.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-featured-work',
   templateUrl: './featured-work.component.html',
-  styleUrls: ['./featured-work.component.css']
+  styleUrls: ['./featured-work.component.css'],
 })
 export class FeaturedWorkComponent implements OnInit {
+  @Input() item?: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -1,15 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styleUrls: ['./homepage.component.css']
+  styleUrls: ['./homepage.component.css'],
 })
 export class HomepageComponent implements OnInit {
-
-  constructor() { }
+  items: any;
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.http
+      .get('http://192.168.1.253:8080/limitourwork')
+      .subscribe((data) => (this.items = data));
   }
-
 }
